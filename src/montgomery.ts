8dd06e2322fbc25431f42f2f3a547025e6a7dce0
JKortmann <j.e.kortmann@gmail.com>
2025-04-28 10:30:01 +0200

    add eslint with typescript support

--- src/montgomery.ts
+++ src/montgomery.ts
@@ -1,11 +1,11 @@
 import {
-  invertPowerOfTwo,
-  bitLength
+    invertPowerOfTwo,
+    bitLength
 } from "./util.js";
 
 // Some useful BigInt constants
 const ZERO = 0n;
-const ONE  = 1n;
+const ONE = 1n;
 
 
 /**
@@ -13,138 +13,138 @@ const ONE  = 1n;
  * and perform Montgomery-reduced arithmetic, modulo a given `base`.
  */
 export interface MontgomeryReductionContext {
-  /**
-   * The modulus of the reduction context
-   */
-  base: bigint,
+    /**
+     * The modulus of the reduction context
+     */
+    base: bigint,
 
-  /**
-   * The exponent of the power of 2 used for `r` (i.e., `r = 2^shift`)
-   */
-  shift: bigint,
+    /**
+     * The exponent of the power of 2 used for `r` (i.e., `r = 2^shift`)
+     */
+    shift: bigint,
 
-  /**
-   * The auxiliary modulus for Montgomery reduction, defined as the smallest power of two greater than `base`
-   */
-  r: bigint,
+    /**
+     * The auxiliary modulus for Montgomery reduction, defined as the smallest power of two greater than `base`
+     */
+    r: bigint,
 
-  /**
-   * The modular inverse of `r` (mod `base`)
-   */
-  rInv: bigint,
+    /**
+     * The modular inverse of `r` (mod `base`)
+     */
+    rInv: bigint,
 
-  /**
-   * The modular inverse of `base` (mod `r`)
-   */
-  baseInv: bigint
+    /**
+     * The modular inverse of `base` (mod `r`)
+     */
+    baseInv: bigint
 }
 
 
 /**
  * Produces a Montgomery reduction context that can be used to define and operate on numbers in Montgomery form
  * for the given base.
- * 
+ *
  * @param {bigint} base The modulus of the reduction context. Must be an odd number.
  * @returns {MontgomeryReductionContext}
  */
 export function getReductionContext(base: bigint): MontgomeryReductionContext {
-  if (!(base & ONE)) throw new Error(`base must be odd`);
+    if (!(base & ONE)) throw new Error(`base must be odd`);
 
-  // Select the auxiliary modulus r to be the smallest power of two greater than the base modulus
-  const numBits = bitLength(base);
-  const littleShift = numBits;
-  const shift = BigInt(littleShift);
-  const r = ONE << shift;
+    // Select the auxiliary modulus r to be the smallest power of two greater than the base modulus
+    const numBits = bitLength(base);
+    const littleShift = numBits;
+    const shift = BigInt(littleShift);
+    const r = ONE << shift;
 
-  // Calculate the modular inverses of r (mod base) and base (mod r)
-  const rInv = invertPowerOfTwo(littleShift, base);
-  const baseInv = r - (((rInv * r - ONE) / base) % r); // From base*baseInv + r*rInv = 1  (mod r)
+    // Calculate the modular inverses of r (mod base) and base (mod r)
+    const rInv = invertPowerOfTwo(littleShift, base);
+    const baseInv = r - (((rInv * r - ONE) / base) % r); // From base*baseInv + r*rInv = 1  (mod r)
 
-  return { base, shift, r, rInv, baseInv };
+    return {base, shift, r, rInv, baseInv};
 }
 
 
 /**
  * Convert the given number into its Montgomery form, according to the given Montgomery reduction context.
- * 
+ *
  * @param {bigint} n Any number
  * @param {MontgomeryReductionContext} ctx The Montgomery reduction context to reduce into
  * @returns {bigint} The Montgomery form of `n`
  */
 export function montgomeryReduce(n: bigint, ctx: MontgomeryReductionContext): bigint {
-  return (n << ctx.shift) % ctx.base;
+    return (n << ctx.shift) % ctx.base;
 }
 
 
 /**
  * Converts the given number _out_ of Montgomery form, according to the given Montgomery reduction context.
- * 
+ *
  * @param {bigint} n A number in Montgomery form
  * @param {MontgomeryReductionContext} ctx The Montgomery reduction context to reduce out of
  * @returns {bigint} The (no longer Montgomery-reduced) number whose Montgomery form was `n`
  */
 export function invMontgomeryReduce(n: bigint, ctx: MontgomeryReductionContext): bigint {
-  return (n * ctx.rInv) % ctx.base;
+    return (n * ctx.rInv) % ctx.base;
 }
 
 
 /**
  * Squares a number in Montgomery form.
- * 
+ *
  * @param {bigint} n A number in Montgomery form
  * @param {MontgomeryReductionContext} ctx The Montgomery reduction context to square within
  * @returns {bigint} The Montgomery-reduced square of `n`
  */
 export function montgomerySqr(n: bigint, ctx: MontgomeryReductionContext): bigint {
-  return montgomeryMul(n, n, ctx);
+    return montgomeryMul(n, n, ctx);
 }
 
 
 /**
  * Multiplies two numbers in Montgomery form.
- * 
+ *
  * @param {bigint} a A number in Montgomery form
  * @param {bigint} b A number in Montgomery form
  * @param {MontgomeryReductionContext} ctx The Montgomery reduction context to multiply within
  * @returns {bigint} The Montgomery-reduced product of `a` and `b`
  */
 export function montgomeryMul(a: bigint, b: bigint, ctx: MontgomeryReductionContext): bigint {
-  if (a === ZERO || b === ZERO) return ZERO;
+    if (a === ZERO || b === ZERO) return ZERO;
 
-  const rm1 = ctx.r - ONE;
-  let unredProduct = a * b;
+    const rm1 = ctx.r - ONE;
+    let unredProduct = a * b;
 
-  const t = (((unredProduct & rm1) * ctx.baseInv) & rm1) * ctx.base;
-  let product = (unredProduct - t) >> ctx.shift;
+    const t = (((unredProduct & rm1) * ctx.baseInv) & rm1) * ctx.base;
+    let product = (unredProduct - t) >> ctx.shift;
 
-  if (product >= ctx.base) {
-    product -= ctx.base;
-  } else if (product < ZERO) {
-    product += ctx.base;
-  }
-  
-  return product;
+    if (product >= ctx.base) {
+        product -= ctx.base;
+    } else if (product < ZERO) {
+        product += ctx.base;
+    }
+
+    return product;
 }
 
 
 /**
  * Calculates `n` to the power of `exp` in Montgomery form.
  * While `n` must be in Montgomery form, `exp` should not.
- * 
+ *
  * @param {bigint} n A number in Montgomery form; the base of the exponentiation
  * @param {bigint} exp Any number (_not_ in Montgomery form)
  * @param {MontgomeryReductionContext} ctx The Montgomery reduction context to exponentiate within
  * @returns {bigint} The Montgomery-reduced result of taking `n` to exponent `exp`
  */
 export function montgomeryPow(n: bigint, exp: bigint, ctx: MontgomeryReductionContext): bigint {
-  // Exponentiation by squaring
-  const expLen = BigInt(bitLength(exp));
-  let result = montgomeryReduce(ONE, ctx);
-  for (let i = ZERO, x = n; i < expLen; ++i, x = montgomerySqr(x, ctx)) {
-    if (exp & (ONE << i)) {
-      result = montgomeryMul(result, x, ctx);
+    // Exponentiation by squaring
+    const expLen = BigInt(bitLength(exp));
+    let result = montgomeryReduce(ONE, ctx);
+    for (let i = ZERO, x = n; i < expLen; ++i, x = montgomerySqr(x, ctx)) {
+        if (exp & (ONE << i)) {
+            result = montgomeryMul(result, x, ctx);
+        }
     }
-  }
 
-  return result;
+    return result;
 }
--- tests/option.test.ts
+++ tests/option.test.ts
@@ -2,75 +2,75 @@ import {primalityTest} from '../src/index.js';
 import {describe, it, expect} from "vitest";
 
 describe("input options", () => {
-  it("should correctly label primes with user-specified numRounds", async () => {
-    const primes = [
-      89n,
-      4145835283301077n
-    ];
-    const results = await Promise.all(primes.map(p =>
-      primalityTest(p, { numRounds: 8 })
-    ));
-    results.forEach((result) => {
-      expect(result.probablePrime).toBe(true)
-    })
-  });
+    it("should correctly label primes with user-specified numRounds", async () => {
+        const primes = [
+            89n,
+            4145835283301077n
+        ];
+        const results = await Promise.all(primes.map(p =>
+            primalityTest(p, {numRounds: 8})
+        ));
+        results.forEach((result) => {
+            expect(result.probablePrime).toBe(true)
+        })
+    });
 
-  it("should correctly label composite numbers with user-specified numRounds", async () => {
-    const composites = [
-      91n,
-      41458352833010723n
-    ];
-    const results = await Promise.all(composites.map(n =>
-      primalityTest(n, { numRounds: 8 })
-    ));
-    results.forEach((result) => {
-      expect(result.probablePrime).toBe(false)
-    })
-  });
+    it("should correctly label composite numbers with user-specified numRounds", async () => {
+        const composites = [
+            91n,
+            41458352833010723n
+        ];
+        const results = await Promise.all(composites.map(n =>
+            primalityTest(n, {numRounds: 8})
+        ));
+        results.forEach((result) => {
+            expect(result.probablePrime).toBe(false)
+        })
+    });
 
-  it("should correctly use a provided array of bases instead of random bases", async () => {
-    // The first eight primes are all strong liars for this composite number...
-    const result1 = await primalityTest(341550071728321n, { bases: [2, 3, 5, 7, 11, 13, 17, 19] })
-    expect(result1.probablePrime).toBe(true)
+    it("should correctly use a provided array of bases instead of random bases", async () => {
+        // The first eight primes are all strong liars for this composite number...
+        const result1 = await primalityTest(341550071728321n, {bases: [2, 3, 5, 7, 11, 13, 17, 19]})
+        expect(result1.probablePrime).toBe(true)
 
-    // ...but 23 is not!
-    const result2 = await primalityTest(341550071728321n, { bases: [23] })
-    expect(result2.probablePrime).toBe(false)
-  });
+        // ...but 23 is not!
+        const result2 = await primalityTest(341550071728321n, {bases: [23]})
+        expect(result2.probablePrime).toBe(false)
+    });
 
-  it("should ignore the numRounds option when bases is specified", async () => {
-    // If it only tests this number with base 2, it should incorrectly label it a probable prime
-    // But if it runs 20 tests with random bases, it will almost certainly be found composite
-    const result = await primalityTest(341550071728321n, { numRounds: 20, bases: [2] })
-    expect(result.probablePrime).toBe(true)
-  });
+    it("should ignore the numRounds option when bases is specified", async () => {
+        // If it only tests this number with base 2, it should incorrectly label it a probable prime
+        // But if it runs 20 tests with random bases, it will almost certainly be found composite
+        const result = await primalityTest(341550071728321n, {numRounds: 20, bases: [2]})
+        expect(result.probablePrime).toBe(true)
+    });
 
-  it("should throw a TypeError when bases option is not an array", async ({expect}) => {
-    // @ts-ignore
-    await expect(primalityTest(113n, { bases: 2 })).rejects.toThrowError(TypeError)
-  });
+    it("should throw a TypeError when bases option is not an array", async ({expect}) => {
+        // @ts-ignore
+        await expect(primalityTest(113n, {bases: 2})).rejects.toThrowError(TypeError)
+    });
 
-  it("should throw a RangeError when one or more provided bases is out of range", async ({expect}) => {
-    await expect(primalityTest(113n, { bases: [1, 2, 3] })).rejects.toThrowError(RangeError)
-    await expect(primalityTest(119n, { bases: [500] })).rejects.toThrowError(RangeError)
-  });
+    it("should throw a RangeError when one or more provided bases is out of range", async ({expect}) => {
+        await expect(primalityTest(113n, {bases: [1, 2, 3]})).rejects.toThrowError(RangeError)
+        await expect(primalityTest(119n, {bases: [500]})).rejects.toThrowError(RangeError)
+    });
 
-  it("should correctly label strong pseudoprimes to a given base", async () => {
-    // Note: 31697 is a strong pseudoprime to base 3, but should still be correctly labeled as composite
-    const result = await primalityTest(31697n, { bases: [3] })
-    expect(result.probablePrime).toBe(false)
-  });
+    it("should correctly label strong pseudoprimes to a given base", async () => {
+        // Note: 31697 is a strong pseudoprime to base 3, but should still be correctly labeled as composite
+        const result = await primalityTest(31697n, {bases: [3]})
+        expect(result.probablePrime).toBe(false)
+    });
 
-  it("should not return a divisor if findDivisor=false", async () => {
-    const composites = [
-      95n,
-      41458352833010691n
-    ];
-    const results = await Promise.all(composites.map(n =>
-      primalityTest(n, { findDivisor: false })
-    ));
-    results.forEach((result) => {
-      expect(result.divisor).toBe(undefined)
-    })
-  });
+    it("should not return a divisor if findDivisor=false", async () => {
+        const composites = [
+            95n,
+            41458352833010691n
+        ];
+        const results = await Promise.all(composites.map(n =>
+            primalityTest(n, {findDivisor: false})
+        ));
+        results.forEach((result) => {
+            expect(result.divisor).toBe(undefined)
+        })
+    });
 });
